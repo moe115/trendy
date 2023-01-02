4b4de0a4a7cb1ab3e380b6bd7f277f75ecf08ac1
moudi115 <moudihmdn@gmail.com>
2023-01-02 15:08:32 +0200

    aa

--- src/Home.js
+++ src/Home.js
@@ -7,28 +7,29 @@ function Home() {
     <div className="home">
     
     <div classNamer="home__container">
-       <img className="home__image" src="https://imgs.search.brave.com/w-rESdGPmxLqt74yVYWh-tBGQLETcBbCSIa4JlpNJGM/rs:fit:1024:768:1/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC9DVlRrT0Nl/LmpwZw" />
-    </div>
+       <img className="home__image" src="https://imgs.search.brave.com/w-rESdGPmxLqt74yVYWh-tBGQLETcBbCSIa4JlpNJGM/rs:fit:1024:768:1/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC9DVlRrT0Nl/LmpwZw" alt="" />
+   
 
 <div className="home__row" >
      <Product />
+     <Product /> <Product /> <Product />
      {/* pr */}
 </div>
 
 
 <div className="home__row" >
      {/* pr */}
-     {/* pr */}
-     {/* pr */}
+     {/* pr */} <Product />
+     {/* pr */} <Product />
 </div>
 
 
 <div className="home__row" >
-    {/* pr */}
+    {/* pr */} <Product />
 </div>
 
 
-
+ </div>
 
     </div>
   );

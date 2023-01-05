7cb45c3389fddd298a91fafe9b77f17e8699ee21
moudi115 <moudihmdn@gmail.com>
2023-01-06 00:11:03 +0200

    f

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,12 +6,17 @@ function Checkout() {
 <div className='checkout__left'>
 
 <img className='checkout__ad' src='https://i.ibb.co/7Sb16st/daenerys-jon-snow-bran-stark-posters-game-of-thrones-season-7-60-2.jpg' />
+<div>
+    <h2 className='checkout__title'>your shopping basket</h2>
+    {/* BasketItem */}
+    {/* BasketItem */}
+    {/* BasketItem */}
+    {/* BasketItem */}
+</div>
+</div>
 
-
-
-
-
-
+<div className='checkout__right'>
+    <h2>totall here</h2>
 </div>
 </div>
 
--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCart from '@mui/icons-material/ShoppingCart';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className="header">
-      
-<img className="header__logo" src="https://i.ibb.co/k0Q7hbx/11.jpg" />
+      <Link to="/">
+      <img className="header__logo" src="https://i.ibb.co/k0Q7hbx/11.jpg" />
+      </Link>
+
  
                 <div className='header__search'>
 
@@ -32,10 +35,12 @@ function Header() {
 <span className='header__optionLineOne'>your</span> 
 <span className='header__optionLineTwo'>prime</span>   
                 </div>
+                <Link  to="/checkout">
 <div className="header__OptionBasket">
     <ShoppingCart /> 
     <span className='header__optionLineOne header__BasketCount'>0</span>
 </div>
+</Link>
                 </div>
     
     

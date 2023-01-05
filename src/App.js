d4d72e9f2f46783192af974b4e356474ed22f332
moudi115 <moudihmdn@gmail.com>
2023-01-05 23:40:05 +0200

    f

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Checkout from './Checkout';
 import Home from './Home';
+import { BrowserRouter ,Routes ,Route } from "react-router-dom";
 
 function App() {
-  return (
-    // bem
-    <div className="app">
-     
-<Header />
-<Home />
-{/* home */}
-
-    </div>
-  );
+  return ( 
+    // bem 
+<BrowserRouter>
+<div className="app">
+  <Routes>
+        <Route path='/' element={<><Header /> <Home />  </>     }  /> 
+        <Route path='/checkout' element={<><Header /> <Checkout />  </>}     />
+  </Routes>
+</div> 
+</BrowserRouter>
+);
 }
 
 export default App;
---
+++ src/Checkout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Checkout.css'
+function Checkout() {
+  return <div className='checkout'>
+ 
+<div className='checkout__left'>
+
+<img className='checkout__ad' src='https://i.ibb.co/7Sb16st/daenerys-jon-snow-bran-stark-posters-game-of-thrones-season-7-60-2.jpg' />
+
+
+
+
+
+
+</div>
+</div>
+
+    
+ 
+}
+
+export default Checkout
